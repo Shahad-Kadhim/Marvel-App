868e1c1935f4843cc49ee85959466faa5e7a0017
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-11-30 22:04:16 +0300

    setup navigation

--- app/src/main/java/com/example/app/marvel/ui/MainActivity.kt
+++ app/src/main/java/com/example/app/marvel/ui/MainActivity.kt
@@ -2,13 +2,36 @@ package com.example.app.marvel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.app.marvel.R
+import com.example.app.marvel.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        ).also { binding ->
+            binding.lifecycleOwner=this
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding.navigation
+            .setupWithNavController(findNavController(R.id.fragment_host))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        findNavController(R.id.fragment_host).navigateUp()
+        return true
+    }
+
 }
